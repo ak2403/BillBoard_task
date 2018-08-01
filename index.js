6c5112bed90499d40953bb4794e270d3ec9dc60d
AKSpikey <arunpasupathi24@gmail.com>
2018-08-01 15:19:00 +1000

    Number of BillBoards photographed atleast once are 1348 & repeated billboards are 176

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var fs = require('fs')
     , filename = './droneInput.txt';
 
-
 fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
+    // getInput = '<vxx<vxx^vxx<>>x';
     getInput = data;
-
     let directions = {
         '^': "north",
         'v': "south",
@@ -14,24 +13,32 @@ fs.readFile(filename, 'utf8', function (err, data) {
         'x': "photograph"
     }
 
-    let reachedPos = [];
+    let reachedPos = [],
+        repeatedShoot = {};
 
-    let tookPhoto = 0,
-        positionDirect = '';
+    var PhotoShoot = 0,
+        positionDirect = '',
+        beforePos = '';
 
     for (let i = 0; i < getInput.length; i++) {
         if (getInput[i] == 'x') {
-            if (positionDirect) {
-                if (reachedPos.indexOf(positionDirect) != -1) {
-                    console.log('repeated')
-                } else {
-                    tookPhoto += 1;
-                    reachedPos.push(positionDirect)
+
+            // console.log(positionDirect)
+            if (positionDirect != '') {
+                PhotoShoot += 1;
+                reachedPos.push(positionDirect);
+                beforePos = positionDirect;
+                positionDirect = '';
+            } else {
+                if (reachedPos.indexOf(beforePos) != -1) {
+                    repeatedShoot[beforePos] ? (repeatedShoot[beforePos] += 1) : (repeatedShoot[beforePos] = 2);
                 }
             }
         } else {
             positionDirect += getInput[i]
         }
     }
-    // console.log(tookPhoto);
+
+    console.log(Object.keys(repeatedShoot).length)
+    console.log("Number of BiiBoard photographed : ", PhotoShoot);
 });
